903cbff275188d9a1e51228a414bda0651f80f4e
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-12-18 19:47:19 +0100

    first commit

--- Eagle.java
+++ Eagle.java
@@ -1,13 +1,39 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
+
 
     public Eagle(String name) {
         super(name);
         this.flying = false;
         this.altitude = 0;
     }
+
+   public void takeOff() {
+    this.flying = true;
+    System.out.println("Hawkeye takes off in the sky.");
+   }
+    public void ascend(int meters){
+      this.altitude += meters;
+        System.out.println(this.getName() + " flies upward, altitude: " + this.altitude );
+
+    } 
+    public void glide(){
+        System.out.println(this.getName() + " glides into the air.");
+
+    } 
+    public void descend(int meters){
+        this.altitude -= meters; 
+        System.out.println("Hawkeye flies upward, altitude: " + this.altitude );
+
+    } 
+ public void land(){
+    if (this.altitude <= 1 ) {
+    System.out.println(this.getName() + " lands on the ground");}
+    System.out.println(this.getName() + " is too high, it can't land");
+
+ }
 
     public int getAltitude() {
         return altitude;
---
+++ Fly.java
@@ -0,0 +1,8 @@
+public interface Fly {
+void takeOff();
+void ascend(int meters);
+void glide();
+void descend(int meters);
+void land();
+  
+} 
